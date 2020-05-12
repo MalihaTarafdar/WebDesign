51095aa07b0f03c9c6c8a6165b5b1b8945664f6b
Maliha Tarafdar <malihatarafdar@yahoo.com>
2020-05-12 02:53:16 -0400

    Complete tab group selection animation

--- news-site/script.js
+++ news-site/script.js
@@ -1,10 +1,24 @@
-const navLinks = document.querySelectorAll('#nav > #links > button')
-let activeLink = document.querySelector('#nav > #links > button.active')
+//====================NAVBAR TABS====================
+const tabs = document.querySelectorAll('#nav > #tabs > button')
+const tabSelection = document.querySelector('#tab-selection')
+let activeTab = document.querySelector('#nav > #tabs > button.active')
 
-for (let i = 0; i < navLinks.length; i++) {
-	navLinks[i].onclick = () => {
-		activeLink.classList.remove('active')
-		navLinks[i].classList.add('active')
-		activeLink = navLinks[i]
-	}
+tabSelection.style.left = activeTab.getBoundingClientRect().left + 'px'
+tabSelection.style.width = activeTab.getBoundingClientRect().width + 'px'
+
+for (let i = 0; i < tabs.length; i++) {
+	tabs[i].onclick = setActiveTab
+}
+
+function setActiveTab() {
+	activeTab.classList.remove('active')
+	this.classList.add('active')
+	activeTab = this
+	tabSelection.style.left = this.getBoundingClientRect().left + 'px'
+	tabSelection.style.width = this.getBoundingClientRect().width + 'px'
+}
+
+window.onresize = () => {
+	tabSelection.style.left = activeTab.getBoundingClientRect().left + 'px'
+	tabSelection.style.width = activeTab.getBoundingClientRect().width + 'px'
 }
