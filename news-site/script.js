4e00ca816951a7d30bba79aea3a76643f217540c
Maliha Tarafdar <malihatarafdar@yahoo.com>
2020-06-03 03:59:43 -0400

    Start creating sidebar

--- news-site/script.js
+++ news-site/script.js
@@ -3,22 +3,27 @@ const tabs = document.querySelectorAll('#nav > #tabs > button')
 const tabSelection = document.querySelector('#tab-selection')
 let activeTab = document.querySelector('#nav > #tabs > button.active')
 
-tabSelection.style.left = activeTab.getBoundingClientRect().left + 'px'
-tabSelection.style.width = activeTab.getBoundingClientRect().width + 'px'
-
 for (let i = 0; i < tabs.length; i++) {
 	tabs[i].onclick = setActiveTab
+}
+
+function setActiveTabBar() {
+	tabSelection.style.left = activeTab.getBoundingClientRect().left + 'px'
+	tabSelection.style.width = activeTab.getBoundingClientRect().width + 'px'
 }
 
 function setActiveTab() {
 	activeTab.classList.remove('active')
 	this.classList.add('active')
 	activeTab = this
-	tabSelection.style.left = this.getBoundingClientRect().left + 'px'
-	tabSelection.style.width = this.getBoundingClientRect().width + 'px'
+	setActiveTabBar()
 }
 
 window.onresize = () => {
-	tabSelection.style.left = activeTab.getBoundingClientRect().left + 'px'
-	tabSelection.style.width = activeTab.getBoundingClientRect().width + 'px'
+	setActiveTabBar()
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+	//set active tab bar
+	setTimeout(setActiveTabBar, 500)
+})
